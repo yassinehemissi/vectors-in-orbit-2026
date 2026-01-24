0bb1ff16d05521e92fdcba11e65607e50f4f0958
Mohamed Yassine Hemissi <hemissiyassine@gmail.com>
2026-01-24 19:31:32 +0100

    Added the tei to blocks module

---
+++ extraction-pipeline/tei-to-blocks/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+from .config import ConverterConfig
+from .core import TEIToBlocks, tei_to_blocks_json
+
+__all__ = ["ConverterConfig", "TEIToBlocks", "tei_to_blocks_json"]
