5960e9f3c2fd5d2edee703f722854abcf465f25f
Mohamed Yassine Hemissi <hemissiyassine@gmail.com>
2026-01-26 03:46:39 +0100

    Fixing Tei to blocks module name

--- extraction-pipeline/tei_to_blocks/__init__.py
+++ extraction-pipeline/tei_to_blocks/__init__.py
@@ -2,5 +2,6 @@ from __future__ import annotations
 
 from .config import ConverterConfig
 from .core import TEIToBlocks, tei_to_blocks_json
+from .text import stable_block_id
 
-__all__ = ["ConverterConfig", "TEIToBlocks", "tei_to_blocks_json"]
+__all__ = ["ConverterConfig", "TEIToBlocks", "tei_to_blocks_json",  "stable_block_id"]
